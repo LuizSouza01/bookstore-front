63bcd7af576b1532253d0e460426272b0733ec92
LuizSouza01 <luizhenri1991@gmail.com>
2021-12-25 12:11:37 -0300

    categoria-read implementado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://luiz-bookstore-api.herokuapp.com'
 };
 
 /*
